3e802388bf2eb059bbaa78afd756ff18e02851bf
vitrh <vinhthong.trinh@hotmail.com>
2020-09-15 19:00:16 +0200

    MergeSort and Quicksort added

---
+++ MergeSort/MergeSort/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[10] {3,1,3,1,5,9,2,6,5,4 };
+            merge_sort(ref test[10]);
+            Console.WriteLine("sortiert:  ");
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+            
+        }
+       private static int[] merge(ref int[] links, ref int[] rechts)
+        {
+            int[] newArray = new int[links.Length + rechts.Length];
+
+            //index = register
+            int indexLinks = 0;
+            int indexRechts = 0;
+            int indexErg = 0;
+
+            while (indexLinks < links.Length && indexRechts < rechts.Length)
+            {
+                if (links[indexLinks] < rechts[indexRechts])
+                {
+                    newArray[indexErg] = links[indexLinks];
+                    indexLinks = indexLinks + 1;
+                }
+                else
+                {
+                    newArray[indexErg] = rechts[indexRechts];
+                    indexRechts = indexRechts + 1;
+                }
+                indexErg = indexErg + 1;
+
+            }
+
+            while (indexLinks < links.Length)
+            {
+                newArray[indexErg] = links[indexLinks];
+                indexLinks += 1;
+                indexErg += 1;
+            }
+
+            while (indexRechts < rechts.Length)
+            {
+                newArray[indexErg] = rechts[indexRechts];
+                indexRechts += 1;
+                indexErg += 1;
+
+            }
+
+            return newArray;
+        }
+        /// <summary>
+        /// Teilt die Arrays in der mitte auf (links & rechts)
+        /// </summary>
+        /// <param name="arr"></param>
+        public void merge_sort(ref int[] arr)
+        {
+            if (arr.Length > 1)
+            {
+                // Teilt array in der mitte
+                int mitte = Convert.ToInt32(arr.Length / 2);
+
+                //linke seite des arrays
+                int[] links = new int[mitte];
+
+                //sortiert linke seite
+                for (int i = 0; i <= links.Length -1; i++)
+                {
+                    links[i] = arr[i];
+                }
+
+
+                int[] rechts = new int[arr.Length - mitte];
+                for (int i = mitte; i <= arr.Length - 1; i++)
+                {
+                    rechts[i - mitte] = arr[i]; 
+                }
+
+                merge_sort(ref links);
+                merge_sort(ref rechts);
+
+                arr = merge(ref links, ref rechts);
+            }
+        }
+
+    }
+}
---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+
+namespace QuickSort
+{
+    class Program
+    {
+        public static void quicksort(int[] array)
+        {
+            quicksort(array, 0, array.Length - 1);
+        }
+        /// <summary>
+        /// das Pivot-Element, wird von der Funktion "teiler" ausgewählt + Methode quicksort wird aufgerufen
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        private static void quicksort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivot = teiler(array, low, high);
+                if (pivot > 1)
+                {
+                    quicksort(array, low, pivot - 1);
+                }
+                if (pivot + 1 < high)
+                {
+                    quicksort(array, pivot + 1, high);
+                }
+
+            }
+        }
+        /// <summary>
+        /// SORTIERT DAS PIVOT. setzt Pivot an richtiger stelle und die Array links und rechts werden mit quicksort sortiert.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        private static int teiler(int[] array, int low, int high)
+        {
+            int pivot = array[low];
+            while (true)
+            {
+
+
+                while (array[low] < pivot)
+                {
+                    low++;
+                }
+                while (array[high] > pivot)
+                {
+                    high--;
+                }
+                if (low < high)
+                {
+                    if (array[low] == array[high])
+                    {
+                        
+                        return high;
+
+                    }
+                    // wenn links kleiner als rechts, dann 
+                    int temp = array[low];
+                    array[low] = array[high];
+                    array[high] = temp;
+                }
+                else
+                {
+                    return high;
+                }
+            }
+
+        }
+       
+        static void Main(string[] args)
+        {
+            int[] newArray = new int[] { 15, 3, 9, 8, 5, 2, 7, 1, 6 }; // neues Array wird erstellt
+            Console.WriteLine("Original Array: ");
+            foreach (var item in newArray)  // Jedes einzelne Element im Array wird in der Konsole ausgegeben
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+            quicksort(newArray, 0, newArray.Length - 1); // Methode wird aufgerufen, Array wird sortiert 
+            Console.WriteLine();
+
+            Console.WriteLine("Sortiertes Array : ");
+            foreach (var item in newArray)
+            {
+                Console.Write(" " +item);
+            }
+        }
+    }
+}
