e5e50749e98842e5a486d63fa4c470542a306fdb
vitrh <vinhthong.trinh@hotmail.com>
2020-09-19 21:29:40 +0200

    binary tree and rekursive seachalgorithm

---
+++ binarytree/binarytree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace binarytree
+{
+    class Program
+    {
+        public class Knoten
+        {
+            public Knoten links { get; set; }
+            public Knoten rechts { get; set; }
+            public int data { get; set; }
+
+        }
+        public class BinaryTree
+        {
+            
+            public void Ausgabe(Knoten root)
+            {
+                Console.WriteLine("Wurzel Wert:{0}", root.data);
+                Console.WriteLine("Linker Wurzel Wert:{0}", root.links.data );
+                Console.WriteLine("Rechter Wurzel Wert:{0}", root.rechts.data);   
+            }
+            public Knoten Einfuegen(Knoten root, string value)
+            {
+                if (root == null)
+                {
+                    root = new Knoten();
+                    root.data = Convert.ToInt32(value);
+                }
+                else if (Convert.ToInt32(value) < root.data)
+                {
+                    root.links = Einfuegen(root.links, value);
+                }
+                else
+                {
+                    root.rechts = Einfuegen(root.rechts, value);
+                }
+                return root;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var tree = new BinaryTree();
+            tree.Einfuegen(new Knoten(), "1");
+            tree.Einfuegen(new Knoten(), "2");
+            tree.Einfuegen(new Knoten(), "1");
+
+            Console.WriteLine(tree);
+    
+
+
+        }
+    }
+}
---
+++ unknownalgorithm4/unknownalgorithm4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace unknownalgorithm4
+{
+   /// <summary>
+   /// Rekursiver Such/Zählalgorithmus
+   /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Res = new int[] {3,4,5,6,3,3,3}; // barlänge ist 7
+            Console.WriteLine(Foo(Res, 3, 0));
+        }
+        /// <summary>
+        /// Vergleicht die gesuchte Zahl mit dem Array Elementen 
+        /// und gibt die Anzahl des vorkommens der gesuchten Zahl im Array aus
+        /// </summary>
+        /// <param name="Bar">Array</param>
+        /// <param name="Star">Gesuchte Zahl</param>
+        /// <param name="Megastar">Laufvariable</param>
+        /// <returns>Anzahl der gesuchten Zahl im Array</returns>
+        public static int Foo(int[] Bar, int Star, int Megastar)
+        {
+           
+            if (Megastar > Bar.Length - 1 ) //megastar ist 0 ->      0 > 7?
+            {
+                return 0;
+            }
+            if (Star == Bar[Megastar]) // 2 ==  ?? -> +1
+            {
+                return (Foo(Bar, Star, Megastar + 1)) + 1;
+            }
+            else
+            {
+                return  (Foo(Bar, Star, Megastar + 1)); 
+            }
+           
+           
+        }
+    }
+}
