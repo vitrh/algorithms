d8cd157193b98b270095a508a8ee56941d0d294a
vitrh <vinhthong.trinh@hotmail.com>
2020-09-19 22:23:17 +0200

    kleine veränderungen

--- unknownalgorithm4/unknownalgorithm4/Program.cs
+++ unknownalgorithm4/unknownalgorithm4/Program.cs
@@ -9,8 +9,8 @@ namespace unknownalgorithm4
     {
         static void Main(string[] args)
         {
-            int[] Res = new int[] {3,4,5,6,3,3,3}; // barlänge ist 7
-            Console.WriteLine(Foo(Res, 3, 0));
+            int[] Res = new int[] {9,4,2,78,2,42,1}; // barlänge ist 7
+            Console.WriteLine("Res = " + Foo(Res, 2, 0));
         }
         /// <summary>
         /// Vergleicht die gesuchte Zahl mit dem Array Elementen 
@@ -20,23 +20,23 @@ namespace unknownalgorithm4
         /// <param name="Star">Gesuchte Zahl</param>
         /// <param name="Megastar">Laufvariable</param>
         /// <returns>Anzahl der gesuchten Zahl im Array</returns>
-        public static int Foo(int[] Bar, int Star, int Megastar)
+        public static int Foo(int[] Bar, int Star, int Megastar) //Komplexität O(n^2)
         {
-           
-            if (Megastar > Bar.Length - 1 ) //megastar ist 0 ->      0 > 7?
+
+            if (Megastar > Bar.Length - 1)  // O(n)
             {
                 return 0;
             }
-            if (Star == Bar[Megastar]) // 2 ==  ?? -> +1
+            if (Star == Bar[Megastar])  // O(n)
             {
                 return (Foo(Bar, Star, Megastar + 1)) + 1;
             }
             else
             {
-                return  (Foo(Bar, Star, Megastar + 1)); 
+                return (Foo(Bar, Star, Megastar + 1));
             }
-           
-           
+
+
         }
     }
 }
