b817851db8a66d8f28b4c6e3e20415184100cee1
vitrh <vinhthong.trinh@hotmail.com>
2020-09-22 00:32:31 +0200

    bubblesort hinzugefügt

---
+++ Bubblesort/Bubblesort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 5, 3, 8, 4, 6 };
+            int[] sortedArray = bubbleSort(x);
+            Console.WriteLine("sorted: " );
+            foreach (var p in x)
+            {
+                Console.Write(p + " ");
+            }
+           
+        }
+        static int[] bubbleSort (int[] array)
+        {
+            int temp;
+            for (int k = 0; k <= array.Length - 2; k++)
+            {
+                for (int i = 0; i <= array.Length - 2; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
